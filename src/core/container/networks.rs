245473a45002b49b3e4129235664c0692c2d3e37
o.nihilist <107046146+onihilist@users.noreply.github.com>
2024-11-26 10:56:27 +0100

    Handle networks

--- src/core/container/networks.rs
+++ src/core/container/networks.rs
@@ -1,11 +1,32 @@
 
 #[derive(Debug, Clone)]
 pub struct RCNetwork {
-    pub name: String,
-    pub driver: Option<String>,
+    pub name: &'static str,
+    pub driver: Option<&'static str>,
     pub driver_opts: Option<std::collections::HashMap<String, String>>,
     pub ipam: Option<RCIpamConfig>,
     pub external: Option<bool>,
+}
+
+#[derive(Debug, Clone)]
+pub enum RCNetworkDriver {
+    BRIDGE,
+    HOST,
+    OVERLAY,
+    MACVLAN,
+    NONE,
+}
+
+impl RCNetworkDriver {
+    pub fn as_str(&self) -> &str {
+        match self {
+            RCNetworkDriver::BRIDGE => "bridge",
+            RCNetworkDriver::HOST => "host",
+            RCNetworkDriver::OVERLAY => "overlay",
+            RCNetworkDriver::MACVLAN => "macvlan",
+            RCNetworkDriver::NONE => "none",
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -19,25 +40,4 @@ pub struct RCIpamConfigEntry {
     pub subnet: Option<String>,
     pub ip_range: Option<String>,
     pub gateway: Option<String>,
-}
-
-pub fn build_network() {
-    let my_network = RCNetwork {
-        name: String::from("my_network"),
-        driver: Some(String::from("bridge")),
-        driver_opts: None,
-        ipam: Some(RCIpamConfig {
-            driver: Some(String::from("default")),
-            config: vec![
-                RCIpamConfigEntry {
-                    subnet: Some(String::from("192.168.1.0/24")),
-                    ip_range: Some(String::from("192.168.1.100/24")),
-                    gateway: Some(String::from("192.168.1.1")),
-                },
-            ],
-        }),
-        external: Some(false),
-    };
-
-    println!("{:?}", my_network);
 }
